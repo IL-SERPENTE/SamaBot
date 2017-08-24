017360a9c42bdb05370eb66ff94d7d70cb46d8e8
BlueSlime <jeremylevilain78@orange.fr>
2017-08-24 14:33:21 +0200

    Publishing source code.

--- src/main/java/net/samagames/tsbot/commands/LinkedCommand.java
+++ src/main/java/net/samagames/tsbot/commands/LinkedCommand.java
@@ -4,8 +4,21 @@ import net.samagames.tsbot.TSBot;
 
 import java.util.UUID;
 
-/**
- * Created by Rigner for project SamaBot.
+/*
+ * This file is part of SamaBot.
+ *
+ * SamaBot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SamaBot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SamaBot.  If not, see <http://www.gnu.org/licenses/>.
  */
 public class LinkedCommand extends AbstractCommand
 {
--- src/main/java/net/samagames/tsbot/commands/TSLinkCommand.java
+++ src/main/java/net/samagames/tsbot/commands/TSLinkCommand.java
@@ -10,8 +10,21 @@ import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
-/**
- * Created by Rigner for project SamaBot.
+/*
+ * This file is part of SamaBot.
+ *
+ * SamaBot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SamaBot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SamaBot.  If not, see <http://www.gnu.org/licenses/>.
  */
 public class TSLinkCommand extends AbstractCommand
 {
